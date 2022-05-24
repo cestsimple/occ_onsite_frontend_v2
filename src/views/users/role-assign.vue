f1812e74f921c53cb7982633216c2f4a5264ea8b
xiangzhe.hou <Cestsimple>
2022-05-24 15:09:35 +0800

    修改malfunction页面表单验证规则

--- src/views/users/role-assign.vue
+++ src/views/users/role-assign.vue
@@ -3,6 +3,11 @@
     <!-- 多选框组 -->
     <el-checkbox-group>
       <!-- 循环选项 -->
+      <el-checkbox v-for="item in roleItems" :key="item.id" :label="item.id">
+        {{
+          item.name
+        }}
+      </el-checkbox>
     </el-checkbox-group>
     <!-- footer插槽 -->
     <el-row slot="footer" type="flex" justify="center">
@@ -15,7 +20,7 @@
 </template>
 
 <script>
-import { getUserById } from '@/api/user'
+import { getUserById, getRole } from '@/api/user'
 import { Message } from 'element-ui'
 export default {
   props: {
@@ -24,13 +29,30 @@ export default {
       default: false
     }
   },
+  data() {
+    return {
+      roleItems: []
+    }
+  },
   methods: {
+    // 传入用户id查询数据
     async getUserInfo(id) {
       try {
+        // 先获取角色列表
+        await this.getRole()
         const res = await getUserById(id)
         this.userInfo = res
       } catch (error) {
         Message.error('获取用户信息失败')
+      }
+    },
+    // 查询角色信息
+    async getRole() {
+      try {
+        const res = await getRole()
+        this.roleItems = res
+      } catch (error) {
+        Message.error('获取角色列表失败')
       }
     }
   }
